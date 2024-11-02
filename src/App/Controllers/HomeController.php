4491326abb8267a5b65e1f6f874d87782b414bf4
Hamidou <globyappouedra@gmail.com>
2024-11-02 11:29:59 +0000

    Adding template Engine and Html Template to my project

--- src/App/Controllers/HomeController.php
+++ src/App/Controllers/HomeController.php
@@ -4,11 +4,22 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use App\Config\Paths;
+use Framework\TemplateEngine;
+
 class HomeController
 {
+    private TemplateEngine $view;
+
+    public function __construct()
+    {
+        $this->view = new TemplateEngine(Paths::VIEW);
+    }
 
     public function home()
     {
-        echo "Home page";
+        echo $this->view->render("index.php", [
+            'title' => 'Home page'
+        ]);
     }
 }
---
+++ src/App/views/partials/_footer.php
@@ -0,0 +1,8 @@
+<!-- Footer -->
+<footer class="container mx-auto my-12 text-center">
+    &copy; Expense Tracker PHP
+</footer>
+<!-- End Footer -->
+</body>
+
+</html>
